139d45078bede9f5979d3bac6133fd16df2ea809
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-09 00:12:37 +0200

    PORTAIL

--- inc/classe.php
+++ inc/classe.php
@@ -1,4 +1,13 @@
 <?php
+require_once "vendor/autoload.php";
+
+use Ovh\Api;
+$apk = "vXjHPaL84Jct1zaB";
+$endpoint = "ovh-eu";
+$ask = "WwSivyMF8kcmKMlsjd6SRCQsmox8XKnO";
+$csk = "k1ung5OPmvb26KtO97wy6R85SyL2ZIVU";
+$ovh = new Api($apk,$ask,$endpoint,$csk);
+
 require_once "control/client.php";
 $client = new client($login);
 $client = $client->info_client($login);
