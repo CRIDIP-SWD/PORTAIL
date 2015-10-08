5e9684aa43cf40e9db1fb8c749d7d3effabfe201
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-09 00:53:03 +0200

    PORTAIL

--- inc/headerbar.php
+++ inc/headerbar.php
@@ -81,10 +81,33 @@
             <ul role="menu" class="dropdown-menu">
                 <li><a href="index.php?view=index&sub=etat_service"><i class="fa fa-server"></i> Etat des Services</a></li>
                 <li><a href="index.php?view=index&sub=chantier"><i class="fa fa-tasks"></i> Chantier</a></li>
-                <li><a data-toggle="modal" data-target="#maj"><i class="fa fa-upload"></i> Note de Mise à jour</a></li>
+                <li><a href="#maj"><i class="fa fa-upload"></i> Note de Mise à jour</a></li>
+
                 <li class="divider"></li>
                 <li><a href="index.php?view=index&sub=support_wiki"><i class="fa fa-life-bouy"></i> Support / Wiki</a></li>
             </ul>
+            <div id="maj" class="modal-block modal-block-md mfp-hide">
+                <section class="panel">
+                    <header class="panel-heading">
+                        <h2 class="panel-title">Are you sure?</h2>
+                    </header>
+                    <div class="panel-body">
+                        <div class="modal-wrapper">
+                            <div class="modal-text">
+                                <p>Are you sure that you want to delete this image?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <footer class="panel-footer">
+                        <div class="row">
+                            <div class="col-md-12 text-right">
+                                <button class="btn btn-primary modal-confirm">Confirm</button>
+                                <button class="btn btn-default modal-dismiss">Cancel</button>
+                            </div>
+                        </div>
+                    </footer>
+                </section>
+            </div>
         </div>
 
         <span class="separator"></span>
--- view/index.php
+++ view/index.php
@@ -158,27 +158,5 @@ include ('inc/header.php');
 <script src="<?= ROOT,ASSETS,JS; ?>dashboard/examples.dashboard.js"></script>
 <script src="<?= ROOT,ASSETS,JS; ?>ui-elements/examples.widgets.js"></script>
 
-<div class="modal-block" id="maj" style="top: -200px">
-    <section class="panel">
-        <header class="panel-heading">
-            <h2 class="panel-title">Are you sure?</h2>
-        </header>
-        <div class="panel-body">
-            <div class="modal-wrapper">
-                <div class="modal-text">
-                    <p>Are you sure that you want to delete this image?</p>
-                </div>
-            </div>
-        </div>
-        <footer class="panel-footer">
-            <div class="row">
-                <div class="col-md-12 text-right">
-                    <button class="btn btn-primary modal-confirm">Confirm</button>
-                    <button class="btn btn-default modal-dismiss">Cancel</button>
-                </div>
-            </div>
-        </footer>
-    </section>
-</div>
 </body>
 </html>
