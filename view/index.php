739c80fc7a92d943585fc839986fcb8a7f8fe1ca
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-09 00:03:55 +0200

    PORTAIL

--- view/index.php
+++ view/index.php
@@ -32,54 +32,6 @@ include ('inc/header.php');
             <!-- start: page -->
             <h1 class="text-center">BIENVENUE SUR VOTRE PANEL UTILISATEUR</h1>
             <h3 class="text-center text-muted">Veuillez selectionner un lien du menu pour avoir un aperçu de votre compte.</h3>
-            <div class="row">
-                <div class="col-md-6">
-                    <section class="panel">
-                        <header class="panel-heading">
-                            <div class="panel-actions">
-                                <a href="#" class="panel-action panel-action-toggle" data-panel-toggle></a>
-                                <a href="#" class="panel-action panel-action-dismiss" data-panel-dismiss></a>
-                            </div>
-
-                            <h2 class="panel-title">Statut des Serveurs</h2>
-                        </header>
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table mb-none">
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Username</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
             <!-- end: page -->
         </section>
     </div>
