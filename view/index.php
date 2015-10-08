34b6b60261ca6723eb7a41990f091da16a49aeba
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-08 19:43:55 +0200

    PORTAIL

--- view/index.php
+++ view/index.php
@@ -13,11 +13,11 @@ include ('inc/header.php');
 
         <section role="main" class="content-body">
             <header class="page-header">
-                <a class="btn" href="index.php?view=cridip">CRIDIP</a>
+                <a class="btn btn-default" href="index.php?view=cridip">CRIDIP</a>
                 <span class="separator"></span>
-                <a class="btn" href="index.php?view=swd">CRIDIP-SWD</a>
+                <a class="btn btn-default" href="index.php?view=swd">CRIDIP-SWD</a>
                 <span class="separator"></span>
-                <a class="btn" href="index.php?view=scpvs">SCPVS</a>
+                <a class="btn btn-default" href="index.php?view=scpvs">SCPVS</a>
 
 
                 <div class="right-wrapper pull-right">
