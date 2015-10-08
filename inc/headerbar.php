0c7eff109dd7592c4841e54d90a35f0203f26ddb
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-09 00:30:00 +0200

    PORTAIL

--- inc/headerbar.php
+++ inc/headerbar.php
@@ -83,21 +83,12 @@
                 </a>
 
                 <div class="dropdown-menu notification-menu large">
-                    <div class="notification-title">
-                        <span class="pull-right label label-default">3</span>
-                        Tasks
-                    </div>
 
                     <div class="content">
                         <ul>
                             <li>
-                                <p class="clearfix mb-xs">
-                                    <span class="message pull-left">Generating Sales Report</span>
-                                    <span class="message pull-right text-dark">60%</span>
-                                </p>
-                                <div class="progress progress-xs light">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                </div>
+                                <a href="">Etat des Services</a>
+                                <a href="">Chantiers</a>
                             </li>
 
                             <li>
