d0efd40cdef784e87aa225d8ac5dd74d26e32587
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-09 10:09:19 +0200

    PORTAIL

--- assets/javascripts/tables/examples.datatables.default.js
+++ assets/javascripts/tables/examples.datatables.default.js
@@ -10,7 +10,7 @@ Theme Version: 	1.4.1
 
 	var datatableInit = function() {
 
-		$('#datatable-default').dataTable({
+		$('#etat_service_serveur').dataTable({
 			"language": {
 				"url": "//cdn.datatables.net/plug-ins/1.10.9/i18n/French.json"
 			}
