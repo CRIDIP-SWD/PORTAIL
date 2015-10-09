9c9a23842f091f18c8cfc0872b61f6f526aeea38
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-09 11:49:45 +0200

    PORTAIL

--- view/index.php
+++ view/index.php
@@ -65,7 +65,7 @@ include ('inc/header.php');
                                         <tr class="gradeX">
                                             <td>
                                                 <?php
-                                                $vps = $ovh->get("/vps");
+                                                $vps = $ovh->get("/vps/vps116895.ovh.net");
                                                 var_dump($vps);
                                                 die();
                                                 ?>
