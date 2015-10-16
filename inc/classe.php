aaa1f849df016015096e1849867403321b80224c
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-17 01:35:44 +0200

    PORTAIL

--- inc/classe.php
+++ inc/classe.php
@@ -8,7 +8,7 @@ $ask = "bDPjuf931jLDmhMMbTLSfatartv43rzB";
 $csk = "is8M5oyHUJNR7UfxB5ToF6wQWAcMPfxn";
 $ovh = new Api($apk,$ask,$endpoint,$csk);
 
-require_once "control/API/ovh_wrapper";
+require_once "control/API/ovh_wrapper.php";
 $ovh = new \Ovh\ovh_wrapper();
 
 
