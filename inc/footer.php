143022cf8e7778354e951cba97b1138ea132b8ea
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-17 00:48:39 +0200

    PORTAIL

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="clearfix">
+    <div class="pull-right">
+        Créer par CRIDIP Solution Web Developpement
+    </div>
+    <div class="pull-left">
+        <span id="year-copy"></span> &copy; <?= NOM_LOGICIEL; ?> - V.<?= VERSION_LOGICIEL; ?>:<?= BUILD_LOGICIEL; ?>
+    </div>
+</footer>
+<!-- END Footer -->
--- view/index.php
+++ view/index.php
@@ -57,16 +57,7 @@
             </div>
             <!-- END Page Content -->
 
-            <!-- Footer -->
-            <footer class="clearfix">
-                <div class="pull-right">
-                    Créer par CRIDIP Solution Web Developpement
-                </div>
-                <div class="pull-left">
-                    <span id="year-copy"></span> &copy; <?= NOM_LOGICIEL; ?> - V.<?= VERSION_LOGICIEL; ?>:<?= BUILD_LOGICIEL; ?>
-                </div>
-            </footer>
-            <!-- END Footer -->
+            <?php include "inc/footer.php"; ?>
         </div>
         <!-- END Main Container -->
     </div>
