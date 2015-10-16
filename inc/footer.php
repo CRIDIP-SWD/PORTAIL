117d5cfeb0c522d9711abcc5fe3be14e81a15d4d
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-17 00:50:45 +0200

    PORTAIL

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="clearfix">
     <div class="pull-right">
-        Créer par CRIDIP Solution Web Developpement
+        CrÃ©er par CRIDIP Solution Web Developpement
     </div>
     <div class="pull-left">
         <span id="year-copy"></span> &copy; <?= NOM_LOGICIEL; ?> - V.<?= VERSION_LOGICIEL; ?>:<?= BUILD_LOGICIEL; ?>
