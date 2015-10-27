ba851d4a35430130ee852d07f908211138646061
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-27 17:14:49 +0100

    PORTAIL

--- view/index.php
+++ view/index.php
@@ -375,7 +375,44 @@
                                     <!-- Tabs Content -->
                                     <div class="tab-content">
                                         <?php if($client['cridip'] == 1){ ?>
-                                        <div class="tab-pane active" id="cridip">Block tabs..</div>
+                                        <div class="tab-pane active" id="cridip">
+                                            <div class="block full">
+                                                <div class="block-title">
+                                                    <h2><strong>Datatables</strong> integration</h2>
+                                                </div>
+                                                <p><a href="https://datatables.net/" target="_blank">DataTables</a> is a plug-in for the Jquery Javascript library. It is a highly flexible tool, based upon the foundations of progressive enhancement, which will add advanced interaction controls to any HTML table. It is integrated with template's design and it offers many features such as on-the-fly filtering and variable length pagination.</p>
+
+                                                <div class="table-responsive">
+                                                    <table id="table-devis" class="table table-vcenter table-condensed table-bordered">
+                                                        <thead>
+                                                            <tr>
+                                                                <th class="text-center">ID</th>
+                                                                <th class="text-center"><i class="gi gi-user"></i></th>
+                                                                <th>Client</th>
+                                                                <th>Email</th>
+                                                                <th>Subscription</th>
+                                                                <th class="text-center">Actions</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td class="text-center">1</td>
+                                                                <td class="text-center"><img src="img/placeholders/avatars/avatar4.jpg" alt="avatar" class="img-circle"></td>
+                                                                <td><a href="javascript:void(0)">client1</a></td>
+                                                                <td>client1@company.com</td>
+                                                                <td><span class="label label-primary">Personal</span></td>
+                                                                <td class="text-center">
+                                                                    <div class="btn-group">
+                                                                        <a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-xs btn-default"><i class="fa fa-pencil"></i></a>
+                                                                        <a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <?php } ?>
                                         <?php if($client['swd'] == 1){ ?>
                                         <div class="tab-pane" id="swd">Profile..</div>
