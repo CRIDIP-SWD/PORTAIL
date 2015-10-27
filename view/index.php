94553fcbaa14c4d07962ae124ba369a448364d37
CRIDIP-SWD <mmockelyn@cridip.com>
2015-10-27 17:15:04 +0100

    PORTAIL

--- view/index.php
+++ view/index.php
@@ -378,9 +378,8 @@
                                         <div class="tab-pane active" id="cridip">
                                             <div class="block full">
                                                 <div class="block-title">
-                                                    <h2><strong>Datatables</strong> integration</h2>
+                                                    <h2><strong>Vos Devis</h2>
                                                 </div>
-                                                <p><a href="https://datatables.net/" target="_blank">DataTables</a> is a plug-in for the Jquery Javascript library. It is a highly flexible tool, based upon the foundations of progressive enhancement, which will add advanced interaction controls to any HTML table. It is integrated with template's design and it offers many features such as on-the-fly filtering and variable length pagination.</p>
 
                                                 <div class="table-responsive">
                                                     <table id="table-devis" class="table table-vcenter table-condensed table-bordered">
