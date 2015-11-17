0e89d7fa47eacda7c71f6751489c8f4b2648f1db
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-17 20:29:28 +0100

    PORTAIL

--- inc/footer.php
+++ inc/footer.php
@@ -23,7 +23,6 @@
 <!--[if IE 8]><script src="<?= ROOT,ASSETS,JS; ?>helpers/excanvas.min.js"></script><![endif]-->
 
 <!-- Include Jquery library from Google's CDN but if something goes wrong get Jquery from local file (Remove 'http:' if you have SSL) -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>!window.jQuery && document.write(decodeURI('%3Cscript src="<?= ROOT,ASSETS,JS; ?>vendor/jquery-1.11.1.min.js"%3E%3C/script%3E'));</script>
 
 <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
@@ -33,5 +32,5 @@
 <script type="text/javascript" src="<?= ROOT,ASSETS,JS; ?>modify.js"></script>
 
 <!-- Google Maps API + Gmaps Plugin, must be loaded in the page you would like to use maps (Remove 'http:' if you have SSL) -->
-<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="<?= ROOT,ASSETS,JS, VENDOR; ?>map.js"></script>
 <script src="<?= ROOT,ASSETS,JS; ?>helpers/gmaps.min.js"></script>
